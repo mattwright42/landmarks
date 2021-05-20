ced937c05a9a39744c02bfcbfdf2ea9e1e2598f7
mattwright42 <matthewbwright@gmail.com>
2021-05-19 21:20:14 -0700

    changes the LandmarkDetails view to generate content dynamically

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -9,14 +9,22 @@ import SwiftUI
 
 struct LandmarkList: View {
     var body: some View {
-        List(landmarks) { landmark in
-            LandmarkRow(landmark: landmark)
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+            
         }
+        
     }
-}
-
-struct LandmarkList_Previews: PreviewProvider {
-    static var previews: some View {
-        LandmarkList()
+    
+    
+    struct LandmarkList_Previews: PreviewProvider {
+        static var previews: some View {
+            LandmarkList()
+        }
     }
 }
